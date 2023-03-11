f9085ffb758b08dee7e4c6fd9340eb8d97a2735a
@IDGS-801-19002027 <19002027@alumnos.utleon.edu.mx>
2023-03-10 22:31:48 -0600

    Sesión Py-MySQL del 09 de marzo del 2023

--- app.py
+++ app.py
@@ -43,19 +43,43 @@ def modificar():
         id = request.args.get('id')
         # Select * from alumnos where id==id    
         alum1=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        create_form.id.data=alum1.id
         create_form.nombre.data=alum1.nombre
         create_form.apellidos.data=alum1.apellidos
         create_form.email.data=alum1.email
 
     if request.method=='POST':
-        id = request.args.get('id')
+        id = create_form.id.data
         # Select * from alumnos where id==id    
         alum=db.session.query(Alumnos).filter(Alumnos.id==id).first()
-        alum1.nombre = create_form.nombre.data
-        alum1.apellidos = create_form.nombre.data
-        alum1.email = create_form.nombre.data
+        alum.nombre = create_form.nombre.data
+        alum.apellidos = create_form.apellidos.data
+        alum.email = create_form.email.data
         db.session.add(alum)
         db.session.commit()
+        return redirect(url_for('ABCompleto'))
+    return render_template('modificar.html', form=create_form)
+
+@app.route("/eliminar", methods=['GET', 'POST'])
+def eliminar():
+    create_form = forms.UserForm(request.form)
+    if request.method=='GET':
+        id = request.args.get('id')
+        # Select * from alumnos where id==id    
+        alum1=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        create_form.id.data=alum1.id
+        create_form.nombre.data=alum1.nombre
+        create_form.apellidos.data=alum1.apellidos
+        create_form.email.data=alum1.email
+
+    if request.method=='POST':
+        id = create_form.id.data
+        # Select * from alumnos where id==id    
+        alum=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        db.session.delete(alum)
+        db.session.commit()
+        return redirect(url_for('ABCompleto'))
+    return render_template('eliminar.html', form=create_form)
 
 
 if __name__ == '__main__':
